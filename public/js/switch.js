3a921fbaa729aaff3c988ccd30f0c29897181aed
Mittsy Tidwell <mtidwell506@gmail.com>
2016-07-28 10:50:16 -0500

    New JS files and Images added

---
+++ public/js/switch.js
@@ -0,0 +1,101 @@
+"use strict";
+
+// 0 = 0%
+// 1 = 10%
+// 2 = 25%
+// 3 = 35%
+// 4 = 50%
+// 5 = FREE
+
+var amount = 60;
+var luckyNumber = Math.floor(Math.random()* 6);
+
+switch (luckyNumber) {
+	case 0:
+		console.log("No discount given, your total is $" + amount);
+		break;
+	case 1:
+		var one_disc = amount - (amount * .10)
+		console.log("You got a 1, your total is $" + one_disc);
+		break;
+	case 2:
+		var one_disc = amount - (amount * .25)
+		console.log("You got a 2, your total is $" + one_disc);
+		break;
+	case 3:
+		var one_disc = amount - (amount * .35)
+		console.log("You got a 3, your total is $" + one_disc);
+		break;
+	case 4:
+		var one_disc = amount - (amount * .5)
+		console.log("You got a 4, your total is $" + one_disc);
+		break;
+	case 5:
+		var one_disc = amount - (amount * .10)
+		console.log("Lucky you, you got a 5. Everything is FREE!!!!!");
+		break;
+	// default:
+	// 	console.log(""):
+}
+
+
+var month = Math.ceil(Math.random()* 12);
+
+switch (month) {
+	case 1:
+		console.log("January");
+		break;
+	case 2:
+		console.log("February");
+		break;
+	case 3:
+		console.log("March");
+		break;
+	case 4:
+		console.log("April");
+		break;
+	case 5:
+		console.log("May");
+		break;
+	case 6:
+		console.log("June");
+		break;
+	case 7:
+		console.log("July");
+		break;
+	case 8:
+		console.log("August");
+		break;
+	case 9:
+		console.log("September");
+		break;
+	case 10:
+		console.log("October");
+		break;
+	case 11:
+		console.log("November");
+		break;
+	case 12:
+		console.log("December");
+		break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
