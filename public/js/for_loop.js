605f7284347f3caf22773f00698e745805cc25a8
Mittsy Tidwell <mtidwell506@gmail.com>
2016-07-29 12:09:44 -0500

    Updated and Added new JS files

---
+++ public/js/for_loop.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// repeats number by number of times
+for (var i = 1; i <= 9; i += 1) {
+	
+	for (var j = 1; j <= 9; j += 1) {
+		j = i.toString();
+		console.log(j.repeat(i));
+	}	
+}
+var lastLine = 0
+lastLine = lastLine.toString();
+console.log(lastLine.repeat(10));
+
+console.log("\n")
+
+
+
+
+// random number * i value results in 
+var randomNumber = Math.floor((Math.random() * 10) + 1);
+
+for (var i = 1; i <= 10; i += 1) {
+	var total = randomNumber * i;
+	console.log(randomNumber + "x" + i + "=" + total);
+}
+
+console.log("\n")
+
+
+
+
+
+// is a number odd or even between 20 and 200 @ random
+for (var i = 1; i <= 10; i += 1) {
+	var randomNumberBetweenTwentyAndTwoHundred = Math.floor((Math.random() * 200) + 20);
+	if (randomNumberBetweenTwentyAndTwoHundred % 2 == 0) {
+		console.log(randomNumberBetweenTwentyAndTwoHundred + " is even");
+	} else {
+		console.log(randomNumberBetweenTwentyAndTwoHundred + " is odd");
+	}
+}
+
+console.log("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
