2d60ceb4c41a8b02c30753ab67bfd21ae9a469c2
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-11 14:31:11 -0500

    jQ update

--- public/jquery/jquery_exercises.js
+++ public/jquery/jquery_exercises.js
@@ -3,26 +3,73 @@
 // $(document).ready(function(){}); checks to see if the DOM is ready
 $(document).ready(function() {
 
-	// // alerts the contents of id num3 when the page is loaded
+	// // ALERT the contents of id num3 when the page is loaded
 	// var contents = $('#num3').html();
 	// alert(contents);
 
-	// // alerts the contents of class lorem when the page is loaded (this will run after the first content var)
+	// // ALERT the contents of class lorem when the page is loaded (this will run after the first content var)
 	// var secondContent = $('.lorem1').html();
 	// alert(secondContent);
 
-	// // changes the background color to yellow after the first 2 alerts
+	// // CSS changes the background color to yellow after the first 2 alerts
 	// $(".lorem2").css('background-color', '#FF0');
 
-	// // gives border to class lorem1
+	// // CSS gives border to class lorem1
 	// $(".lorem1").css('border', '1px solid black');
 
-	// // changes font size for li element
+	// // CSS changes font size for li element
 	// $("li").css("font-size", "20px");
 
-	// // mulit selectors, changing the background to yellow
+	// // CSS mulit selectors, changing the background to yellow
 	// $("h1, p, li").css("background-color", "#FF0");
 
-	// * collects everything in the HTML file and sets a red border around it
-	$("*").css("border", "1px solid #F00");
+	// // CSS * collects everything in the HTML file and sets a red border around it
+	// $("*").css("border", "1px solid #F00");
+
+	// // CSS center aligns text for lorem2
+	// $(".lorem2").css("text-align", "center");
+
+	// // CLICK adding a click event to h1
+	// $("#codeup").click(function() {
+	// 	alert("h1 with id 'codeup' was clicked");
+	// });
+	
+	// HOVER Changes the background color when hovering over h1
+	$('#codeup').hover (
+		function() {
+			$(this).css('background-color', '#FF0');	
+		},
+		function() {
+			$(this).css('background-color', '#FFF');
+		}
+	);
+
+	// DOUBLE CLICK EVENT
+	$("p").dblclick(function(){
+		$("p").css("font-size", "18px");
+	});
+
+	// HOVER EVENT
+	$('li').hover (
+		function() {
+			$(this).css("color", "red");
+		},
+		function() {
+			$(this).css("color", "black");
+		}
+	);
+	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
