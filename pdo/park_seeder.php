2c0bd5ff9089235ac1acd9b8b9cfa70bf7c2cfc6
Mittsy Tidwell <mtidwell506@gmail.com>
2016-09-25 22:09:52 -0500

    page numbers listed, working, and a little styling completed. Still need to add in the < and > buttons to go from page to page

--- pdo/park_seeder.php
+++ pdo/park_seeder.php
@@ -16,18 +16,22 @@ $parks = [
 	['name' => 'American Samoa','location' => 'American Samoa','date_established' => '1988-10-31','area_in_acres' => '9000.00',],
 	['name' => 'Arches','location' => 'Utah','date_established' => '1929-04-12','area_in_acres' => '76518.98',],
 	['name' => 'Badlands','location' => 'South Dakota','date_established' => '1978-11-10','area_in_acres' => '242755.94',],
+
 	['name' => 'Big Bend','location' => 'Texas','date_established' => '1944-06-12','area_in_acres' => '801163.21',],
 	['name' => 'Biscayne','location' => 'Florida','date_established' => '1980-06-28','area_in_acres' => '172924.07',],
 	['name' => 'Black Canyon of the Gunnison','location' => 'Colorado','date_established' => '1999-10-21','area_in_acres' => '32950.03',],
 	['name' => 'Bryce Canyon','location' => 'Utah','date_established' => '1928-02-25','area_in_acres' => '35835.08',],
+	
 	['name' => 'Canyonlands','location' => 'Utah','date_established' => '1964-09-12','area_in_acres' => '337597.83',],
 	['name' => 'Capitol Reef','location' => 'Utah','date_established' => '1971-12-18','area_in_acres' => '241904.26',],
 	['name' => 'Carlsbad Caverns','location' => 'New Mexico','date_established' => '1930-05-14','area_in_acres' => '46766.45',],
 	['name' => 'Channel Islands','location' => 'California','date_established' => '1980-03-05','area_in_acres' => '249561.00',],
+	
 	['name' => 'Congaree','location' => 'South Carolina','date_established' => '2003-11-10','area_in_acres' => '26545.86',],
 	['name' => 'Crater Lake','location' => 'Oregon','date_established' => '1902-05-22','area_in_acres' => '183224.05',],
 	['name' => 'Cuyahoga Valley','location' => 'Ohio','date_established' => '2000-10-11','area_in_acres' => '32860.73',],
 	['name' => 'Death Valley','location' => 'California','date_established' => '1994-10-31','area_in_acres' => '3372401.96',],
+	
 	['name' => 'Denali','location' => 'Alaska','date_established' => '1917-02-26','area_in_acres' => '4740911.72',],
 	['name' => 'Dry Tortugas','location' => 'Florida','date_established' => '1992-10-26','area_in_acres' => '64701.22',],
 	['name' => 'Everglades','location' => 'Florida','date_established' => '1934-05-30','area_in_acres' => '1508537.90',],
--- public/php/national_parks.php
+++ public/php/national_parks.php
@@ -1,103 +1,138 @@
-<!-- 
-Inside ~/vagrant-lamp/sites/codeup.dev/public create a page called national_parks.php that lists the national parks from your database. On each page load, it should retrieve all records from the database and display them.
-
-Modify your query to load only four parks at a time. Use a parameter in $_GET to determine which page the user is on and load only the appropriate parks for that page.
-
-
-Modify your page to add links to go to the next or previous page(s).
-
-Add some logic to determine whether or not to show the next and/or previous page links. 
--->
 <?php 
-	// fetches the constants and gives to the connect file in order to connect to the DB
+
+	// Modify your page to add links to go to the next or previous page(s).
+
+	// Add some logic to determine whether or not to show the next and/or previous page links.
+
+
+
+
+
+	// uses both files in order to connect and run database for the national parks query
 	require_once __DIR__ . "/../../pdo/db_constants_nat_park.php";
 	require_once __DIR__ . "/../../pdo/db_connect.php";
 
-
+	// $dbc parameter is being pulled from db_connect.php file
 	function pageController($dbc) {
-		// Constant set for amount of parks displayed
 		define('RPP', 4);
 
-		$value = [];
-		$offBy = 0;
+		// used to determine where to start on the query
+		$offby = (!empty($_GET)) ? (($_GET['page'] - 1) * RPP) : 0;
 
-		// $dbc is coming from db_connect.php
-		function getParks($dbc, $offBy){
-			// Modify your query to load only four parks at a time. 
-			$query = "SELECT * FROM national_parks LIMIT " . RPP . " OFFSET " . $offBy . ";";
+		// SQL query set to get all info from array and display. echo bellow is to ensure that the SQL is running and reading correctly and double checking for spacing.
+        $sqlQuery = "SELECT * FROM national_parks;";
+		// echo $stm;
 
-			// $stm can be named anything we want
-			// query() is a function from $dbc
-			// echo $query;
-			$stm = $dbc->query($query);
+		// $dbc is an instance, query() is a function call that uses SQL to gather everything from the database
+		$stm = $dbc->query($sqlQuery);
 
-			// $rows is just a name
-			// $stm is called 
-			// fetchAll will gather everything from the results of $stm
-			$value = $stm->fetchAll(PDO::FETCH_ASSOC);
-			return $value;
-		}
+        // limit to 4 per page
+        $sqlLimit = $dbc->query("SELECT * FROM national_parks LIMIT " . RPP . " OFFSET " . $offby . ";");
 
-		// calling the getParks function
-		$parks = getParks($dbc, $offBy);
+		// fetchAll() - returns an array containing all of the result set rows
+        // PDO::FETCH_ASSOC - returns an array indexed by column name as returned in your result set
+		$rows = $sqlLimit->fetchAll(PDO::FETCH_ASSOC);
 
-		foreach($parks as $park){
-			// print_r($park);
-			echo "• " . $park['name']. " --- ".$park['location']. " --- ".$park['date_established']. " --- ".$park['area_in_acres'];
-			echo "<br>";
-		}
+        // count is used in HTML to determine what number will be displayed for the number of pages
+        $count = 1;
 
-		function getPage() {
-			if (isset($_GET['page'])) {
-				$value = $_GET['page'];
-			} else {
-				$value = 'Not defined';
-			}
-		}
-
-		// calling the getPage function
-		getPage();
-
-		return $value;
+		return [
+			// set key => value pairs in order to call them in the HTML
+			// gathers the SQL info and displays on page
+			// 'parks' => ,
+			// determines how many parks are left in the query
+			'parks' => $rows,
+            'parkCount' => $stm->rowCount(),
+            'RPP' => RPP,
+            'page' => $offby,
+            'count' => $count
+		];
 	}
-	extract(pageController($dbc))
-?>  
+	extract(pageController($dbc));
+?>
 
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<html lang="en">
+	    <head>
+	        <meta charset="utf-8">
+	        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	        <meta name="viewport" content="width=device-width, initial-scale=1">
+	        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	
+	        <title>National Parks</title>
+	
+	        <!-- Bootstrap core CSS -->
+	        <link rel="stylesheet" href="/bootstrap-3.3.6-dist/css/bootstrap.css">
 
-        <title></title>
+            <style>
+                body {
+                    background-image: url("national_parks_background.jpg");
+                    background: cover;
+                    color: white;
+                    padding-top: 5%;
+                }
 
-        <!-- Bootstrap core CSS -->
-        <link rel="stylesheet" href="/bootstrap-3.3.6-dist/css/bootstrap.css">
-    </head>
+                #pageNum {
+                    font-size: 20px;
+                }
 
-    <body>
-		<div>
-			<ul class="pagination">
-				<li>
-					<a href="http://codeup.dev/php/national_parks.php?$value="></a>
-				</li>
-			</ul>
-		</div>	
-   
+                a {
+                    color: white;
+                }
 
+            </style>
+	    </head>
+	
+	    <body>
+            <!-- entire container for the page -->
+			<div class="container">
+                <h1>National Parks</h1>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                <table class="table table-bordered">
+                    <!-- header for the table -->
+                    <th><h4>Name</h4></th>
+                    <th><h4>Location</h4></th>
+                    <th><h4>Established</h4></th>
+                    <th><h4>Size</h4></th>
 
+                    <!-- data for the table -->
+                    <?php foreach ($parks as $park) { ?>
+                        <tr>
+                            <td><?= $park['name'] ?></td>
+                            <td><?= $park['location'] ?></td>
+                            <td><?= $park['date_established'] ?></td>
+                            <td><?= $park['area_in_acres'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </table>
 
-    <!-- AJAX and JS
-    ================================================== -->
-    <!-- bootstarp JS -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                <!-- anchors for pages -->
+				<ul class="pagination">
+                    <div id="pageNum">
+                        <!-- makes anchor for most of array only ones left out are the remainder of the division -->
+                        <?php for ($i = 1; $i <= $parkCount / RPP; $i += 1) {?>
+    				        <a href="http://codeup.dev/php/national_parks.php?page=<?= $i ?>"> <?= $count++ ?> </a>
+                        <?php } ?>
 
-    </body>
-</html>
+                        <!-- makes anchor for the remainder of the division -->
+                        <?php if (($parkCount / RPP) - $i < RPP) { ?>
+                            <a href="http://codeup.dev/php/national_parks.php?page=<?= $i ?>"> <?= $count++ ?> </a>
+                        <?php } ?>
+                    </div>
+				</ul>
+			</div>
+	
+	
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	
+	
+	    <!-- AJAX and JS
+	    ================================================== -->
+	    <!-- bootstarp JS -->
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	    </body>
+	</html>
